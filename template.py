d0790eb05fe94d5604bfeb207ed8a69020430729
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-05-03 15:04:34 +0300

    requirements

--- template.py
+++ template.py
@@ -27,6 +27,7 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     f"src/{project_name}/exception/__init__.py",
     f"src/{project_name}/logger/__init__.py",
+    f"src/{project_name}/configuration/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "requirements.txt",
